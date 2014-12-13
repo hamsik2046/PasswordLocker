4c2e49b8355cb2b676741ba2d4665dece726cf5e
hamsik2046 <nixoul@126.com>
2014-12-13 09:52:29 +0800

    test

--- PasswordLocker/src/com/hamsik2046/password/activity/PasswordListActivity.java
+++ PasswordLocker/src/com/hamsik2046/password/activity/PasswordListActivity.java
@@ -1,9 +1,13 @@
 package com.hamsik2046.password.activity;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class PasswordListActivity extends Activity {
 
-	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    }	
 
 }
