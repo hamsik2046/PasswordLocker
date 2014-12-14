471efde6a720f1b3a7f8c885878bc39994c78102
hamsik2046 <nixoul@126.com>
2014-12-14 12:07:32 +0800

    Signed-off-by: hamsik2046 <nixoul@126.com>

--- PasswordLocker/src/com/hamsik2046/password/MainActivity.java
+++ PasswordLocker/src/com/hamsik2046/password/MainActivity.java
@@ -1,106 +1,95 @@
 package com.hamsik2046.password;
 
+import java.util.ArrayList;
+import java.util.List;
 
-
-import com.gc.materialdesign.views.ButtonFloat;
-import com.gc.materialdesign.widgets.Dialog;
-import com.hamsik2046.password.activity.PasswordListActivity;
-
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.text.InputType;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnClickListener{
-	
-	private ButtonFloat buttonFloat;  //DZT的button
-	private SharedPreferences data;
-	private Boolean isLogined = false;
+import com.hamsik2046.password.activity.PasswordListActivity;
+import com.hamsik2046.password.view.SingleInputFormActivity;
+import com.hamsik2046.password.view.Step;
+import com.hamsik2046.password.view.TextStep;
+
+public class MainActivity extends SingleInputFormActivity {
+
+	private SharedPreferences userInfo;
+	private Boolean isFirst = true; // 是否第一次使用：是则设置密码，否则直接登录(默认为)
 	private String password;
-	private Dialog dialog;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		initView();
-		initData();
-		
-	}
-
-	private void initView(){
-		buttonFloat = (ButtonFloat) findViewById(R.id.buttonFloat);
-		buttonFloat.setOnClickListener(this);
-		dialog = new Dialog(MainActivity.this, "Set Login Password", null);
-	}
-	
-	private void initData(){
-		data = getSharedPreferences("passwordlocker.data", Context.MODE_PRIVATE);
-		password = data.getString("login_password", null);
-		isLogined = password == null ? false : true;
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		userInfo = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+		password = userInfo.getString("password", null);
+		if (password != null && !password.equals("")) {
+			isFirst = false;
 		}
-		return super.onOptionsItemSelected(item);
 	}
 
 	@Override
-	public void onClick(View view) {
-		switch (view.getId()) {
-		case R.id.buttonFloat:
-			if (isLogined) {
-				showLoginDialog();
+	protected List<Step> getSteps(Context context) {
+		
+		if (isFirst) {
+			List<Step> steps1 = new ArrayList<Step>();
+			steps1.add(new TextStep(context, "input_pwd",
+					InputType.TYPE_TEXT_VARIATION_PASSWORD, R.string.password_set,
+					R.string.password_error, R.string.password_details));
+			steps1.add(new TextStep(context, "confirm_pwd",
+					InputType.TYPE_TEXT_VARIATION_PASSWORD, R.string.password_confirm,
+					R.string.password_error, R.string.password_details));
+			return steps1;
+		} else {
+			List<Step> steps2 = new ArrayList<Step>();
+			steps2.add(new TextStep(context, "login_pwd",
+					InputType.TYPE_TEXT_VARIATION_PASSWORD, R.string.password_login,
+					R.string.password_error, R.string.password_details));
+			return steps2;
+		}
+
+	}
+
+	@Override
+	protected void onFormFinished(Bundle data) {
+		if (isFirst) {
+			String password_set = TextStep.text(data, "input_pwd");
+			String password_cofirm = TextStep.text(data, "confirm_pwd");
+			if (password_set.equals(password_cofirm)) {
+				Intent intent = new Intent(MainActivity.this,
+						PasswordListActivity.class);
+				startActivity(intent);
+				finish();
+				 userInfo.edit().putString("password",
+				 password_cofirm).commit();
+			} else {
+				finish();
+				Intent intent = new Intent(MainActivity.this,
+						MainActivity.class);
+				startActivity(intent);
+				Toast.makeText(MainActivity.this, "twice input not equal", Toast.LENGTH_SHORT).show();
+			}
+		} else {
+			String password_login = TextStep.text(data, "login_pwd");
+			if (password_login.equals(password)) {
+				Intent intent = new Intent(MainActivity.this,
+						PasswordListActivity.class);
+				startActivity(intent);
+				finish();
 			}else {
-				showSetLoginPasswordDialog();
+				Toast.makeText(MainActivity.this, "wrong password", Toast.LENGTH_SHORT).show();
+				finish();
+				Intent intent = new Intent(MainActivity.this,
+						MainActivity.class);
+				startActivity(intent);
 			}
-			
-			break;
-
-		default:
-			break;
 		}
-	}
-	
-	private void showLoginDialog(){
+
 		
-	}
-	
-	private void showSetLoginPasswordDialog(){
-		dialog.setOnConfirmButtonClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View arg0) {
-				password = dialog.checkEditTextContent();
-				if (!TextUtils.isEmpty(password)) {
-					dialog.dismiss();
-					Intent intent = new Intent(MainActivity.this,PasswordListActivity.class);
-					//cache password
-					data.edit().putString("login_pwd", password).commit();
-				}
-			}
-		});
-		dialog.show();
 	}
 	
 }
--- PasswordLocker/src/com/hamsik2046/password/activity/PasswordListActivity.java
+++ PasswordLocker/src/com/hamsik2046/password/activity/PasswordListActivity.java
@@ -1,5 +1,7 @@
 package com.hamsik2046.password.activity;
 
+import com.hamsik2046.password.R;
+
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -8,6 +10,7 @@ public class PasswordListActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.activity_password_list);
     }	
 
 }
